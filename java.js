a2f978114b030e9960a00d513ace92121b373d82
allmightyfab <151810543+allmightyfab@users.noreply.github.com>
2024-01-17 11:31:21 -0600

    test3

--- java.js
+++ java.js
@@ -2,8 +2,9 @@
 
     alert("Hello?")
     likes = prompt("What is your favorite food?")
+    let count=1
     num= prompt("What is your favorite number?")
         num=parseInt(num);
-
+count =count+num
 console.log("so you like "+pie)
-
+console.log(count)
