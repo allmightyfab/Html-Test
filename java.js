6f664e82456e80609848a0bc1585d893e3b56cdb
allmightyfab <151810543+allmightyfab@users.noreply.github.com>
2024-01-17 11:21:53 -0600

    test

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 //Fabien Selemani-1/17/24
 <script>
+    alert("Hello?")
     likes = prompt("What is your favorite food?")
     num= prompt("What is your favorite number?")
         num=parseInt(num);
